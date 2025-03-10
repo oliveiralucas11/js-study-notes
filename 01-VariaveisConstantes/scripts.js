d108de08ec1634e15ce548b80e04cd0a6ce5db34
Lucas Oliveira Bueno <93354611+oliveiralucas11@users.noreply.github.com>
2025-03-10 14:25:55 -0400

    variaveis e constantes

--- 01-VariaveisConstantes/scripts.js
+++ 01-VariaveisConstantes/scripts.js
@@ -13,3 +13,22 @@ console.log(email) // Vai retornar o valor na variavel, sendo ela uma string
 email = "joao@email.com"
 console.log(email) // Agora o valor a ser mostrado sera o alterado e não o definido anteriormente. 
 
+//Declarar uma variavel let sem valor
+let user2 // Essa variavel nao permite que outra seja criada com o mesmo nome sobreescrevendo a si mesma uma vez colocada o nome não pode ser usada em outra variavel a nao ser que seja alterada.
+console.log(user2)
+
+//Declarar variavel let com valor
+let email2 = "lucasoo@gmail.com"
+console.log(email2)
+email2 = "joaozinho@gmail.com"
+console.log(email2) // Mesma funcionalidade da var padrão mudando somente o fato de nao poder criar outra variavel com mesmo nome sobreescrevendo a mesma.
+
+//-------------------------------------------------------------------
+//Constantes são valores reservados na memoria porém eles são fixos nao podendo serem alterados ao longo do código, como é feito nas variaveis normais.
+
+//Declarar constante com valor
+const number = 32
+console.log(number) // O conteudo da constante é fixa e não pode ser alterado ex: number=40 , dara erro.
+
+
+
