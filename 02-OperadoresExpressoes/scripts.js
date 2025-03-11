a78e7359431425ce32e624dd50f3105f53870c50
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2025-03-11 10:00:26 -0400

    Operadores aritimeticos

---
+++ 02-OperadoresExpressoes/scripts.js
@@ -0,0 +1,31 @@
+// Operadores Aritimeticos
+
+// Soma
+let soma1 = 5 + 3
+console.log(`O resultado da soma é de: ${soma1}`)
+console.log("O resultado da soma é de: ", 5 + 3)
+console.log("O resultado da soma é de: ", soma1)
+
+// Subtração
+let subtracao = 33 - 10
+console.log(`O resultado da subtracao é de: ${subtracao}`)
+console.log("O resultado da subtracao é de: ", 5 - 3)
+console.log("O resultado da subtracao é de: ", subtracao)
+
+// Multiplicação
+let multiplicacao = 32 * 4
+console.log(`O resultado da multiplicacao é de: ${multiplicacao}`)
+console.log("O resultado da multiplicacao é de: ", 5 * 3)
+console.log("O resultado da multiplicacao é de: ", multiplicacao)
+
+// Divisao
+let divisao = 15 / 2
+console.log(`O resultado da divisao é de: ${divisao}`)
+console.log("O resultado da divisao é de: ", 12 / 2)
+console.log("O resultado da divisao é de: ", divisao)
+
+// Potenciação
+console.log("Exponencial: ", 3 ** 3) // 3 elevado a 3
+//-------------------------------------------------------------------------------
+
+// Operadores de Incremento
