32d37f5ace7cb570d9fc83f5df86a44a212c04d5
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2025-03-11 10:09:33 -0400

    incremento e decremento

--- 02-OperadoresExpressoes/scripts.js
+++ 02-OperadoresExpressoes/scripts.js
@@ -29,3 +29,26 @@ console.log("Exponencial: ", 3 ** 3) // 3 elevado a 3
 //-------------------------------------------------------------------------------
 
 // Operadores de Incremento
+let number = 10
+number = number + 10 // Incrementando mais 10 na variavel number
+number++ // ++ operador de incremento colocando sempre de 1 em 1, com o sinal depois ele incrementa após e não antes.
+
+//Incrementa após por isso nao mostra no console, usando ++ após o nome
+console.log(number++)
+console.log(number)
+
+//Incrementa antes, aparecendo antes no console
+console.log(++number)
+console.log(number)
+
+//Decremento
+console.log("Decremento após: ", number--)
+console.log("Decremento antes: ", --number)
+
+//Incrementar mais de um
+number += 20
+console.log(number)
+
+//Decrementar mais de um
+number -= 5
+console.log(number)
