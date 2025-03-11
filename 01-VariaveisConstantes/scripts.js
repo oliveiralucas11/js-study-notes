dc5e620f295820ca65d54f836eea0410810661fd
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2025-03-10 20:19:55 -0400

    tipos de variaveis , concatenação , string literals, template literals, interpolação, tipos number , float, NaN , boolean, undefined, null, conversão de tipos e coerção de tipos

--- 01-VariaveisConstantes/scripts.js
+++ 01-VariaveisConstantes/scripts.js
@@ -76,3 +76,112 @@ let lastName = "Bueno"
 let product_name = "Teclado"
 let last_name = "Lucas"
 let first_name = "Oliveira"
+//--------------------------------------------------------------------------------------
+
+// Tipos de Variaveis
+// Tipo string : que armazena somente textos em aspas dulas
+let myName = "Lucas Oliveira" // tipo string com aspas duplas ou simples
+console.log(myName)
+console.log(typeof myName) // typeof usado pra saber qual o tipo da variavel que está sendo armazenada
+console.log("Uma string com aspas")
+console.log('uma string com aspas simples')
+
+//Quando utilizar uma ou outra?
+console.log('Uma string com "aspas duplas" dentro de aspas simples')
+
+console.log("Uma string com 'aspas simples' dentro de aspas duplas")
+
+console.log(`
+  Uma string com acento grave permite
+  multiplas linhas.
+  `)
+
+  // Template literals (template strings, interpolação de strings)
+  let userData = "Lucas Oliveira"
+  let userDataEmail = "lucasoliveria@email.com"
+  
+  // Podemos passar mais de um parametro
+  console.log(userData, userDataEmail)
+
+  // Concatenar texto
+  let message = "Ola, " + userData + ". Você conectou com o email: " + userDataEmail
+  console.log(message)
+
+  // Template literals (Interpolação)
+  console.log(`Ola, ${userData}. Você conectou com o email: ${userDataEmail}`)
+  //--------------------------------------------------------------------------------------
+
+
+  // Tipo Number
+
+  // Inteiro positivo
+  let integerPositive = 5
+  console.log(integerPositive)
+  console.log(typeof integerPositive)
+  console.log(5)
+
+  // Inteiro negativo
+  let integerNegative = -5
+  console.log(integerNegative)
+  console.log(typeof integerNegative)
+  console.log(-5)
+
+  // Numeros reais ou float
+  let floatNumber = 32.21
+  console.log(floatNumber)
+  console.log(typeof floatNumber)
+  console.log(22.22)
+
+  // NaN - Not a Number
+  console.log(12.5 / "Rodrigo") // esse valor vai retornar NaN, pois aqui estamos tentando dividir um numero por um texto e isso nao é possivel.
+  //-------------------------------------------------------------------------------------------
+
+
+  // Tipo Boolean
+  console.log(true) // verdadeiro
+  console.log(false) // Falso
+
+  let isLoading = true 
+  console.log(isLoading)
+  console.log(typeof isLoading)
+  //----------------------------------------------------------------------------------------------
+
+
+  // Tipo undefined e null
+
+  //Undefined (indefinido) tipo de dado que o javascript atribui de forma automatica para uma variavel que ainda nao tem valor.
+  let emptiness
+  console.log("O valor é: ", emptiness) // Valor recebido sera definido com undefined até que eu mude o seu valor.
+
+  // Null é um tipo de dado pra dizer que o conteudo de uma variavel é literalmente vazio/nulo
+  let empty = null
+  console.log("O valor dessa var é: ", empty)
+  console.log(typeof empty)
+  //--------------------------------------------------------------------------------------------------
+
+  
+  /* Conversão de tipos (type casting ou type conversion):
+    ocorre quando você explicitamente transforma um valor de um tipo para o outro. 
+    Isso é feito de forma consciente, usando funções ou metodos especificos para realizar a conversao.
+  */
+  let value = "9"
+  console.log(typeof value)
+  console.log(typeof Number(value)) // Fazendo uma conversao do tipo string para tipo Number
+
+  let age = 18
+  console.log(typeof age)
+  console.log(String(age)) // Convertendo do jeito tradicional
+  console.log(age.toString()) // Convertendo number para um texto de um outro jeito
+
+  let options = 1 
+  console.log(typeof options)
+  console.log(Boolean(options)) // Converter numero para boolean sendo numero 1 como verdadeiro e 0 falso.
+
+    
+/*  Coerção de tipos:
+    acontece de forma automatica (implicitamente). O Javascript tenta automaticamente converter um dos valores para um tipo compativel
+    antes de realizar a operação. 
+*/
+
+  console.log("10" + 5) // O javascript vai tentar converter automaticamente para fazer a operação, convertendo o 5 fazendo ele passar a ser um texto e concatenando os 2 virando 105
+
