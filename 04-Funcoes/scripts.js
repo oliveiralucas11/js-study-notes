4a8d0a150b6b78a3cb7a41b3a3528ccc0c3de7f0
Lucas Oliveira <lucasgalvaooliveira12@gmail.com>
2025-03-11 21:55:05 -0400

    mais algumas coisas sobre funções que nao deveriam nem existir na programação, js é estranho as vezes...

--- 04-Funcoes/scripts.js
+++ 04-Funcoes/scripts.js
@@ -46,3 +46,91 @@ let response = sum(10,20)
 console.log(response) // Para recebermos o retorno da função devemos armazenar a sua chamada dentro de uma variavel e chama-la pela variavel.
 
 console.log(sum(3,6)) // Ou podemos chamar direto no console log sem precisar armazenar primeiro em uma variavel e chama-la da variavel.
+//-----------------------------------------------------------------------------------
+
+/* 
+  Comentario de documentação em Javascript (sintaxe de JSDoc).
+  O JSDoc é um padrao para incorporar documentação no codigo-fonte a 
+  partir desses comentarios.
+ */
+
+
+  /**
+   * Authenticates the user
+   * 
+   * @param {String} email user email.
+   * @param {String} password more than 6 characters.
+   * @returns {Number} user id.
+   */
+  function signIN (email, password){
+    // Fluxo de autenticação do usuário.
+
+
+    return 7
+  }
+
+
+  signIN("lucas@email.com", "123456")
+//-------------------------------------------------------------------------------------
+
+//Função anonima (função que não possui nome)
+// Muito comum guardar função dentro de variavel usando função anonima porem nesse caso não guardamos o retorno dela em uma variavel , mas sim a função inteira.
+
+// Ela é usada imediatamente e guardada em uma variavel.
+const showMessage = function (){ // criando a função diretamente de uma variavel/constante tornando ela uma função anonima sem nome.
+  return "Ola, Lucas"
+
+}
+
+console.log(showMessage())
+
+const showMessage1 = function (name, data){
+
+  return "Ola, " + name + data
+
+}
+
+console.log(showMessage1("Lucas", " hoje é dia 11/03"))
+//----------------------------------------------------------------------------------------
+
+//Arrow Function (função de seta)
+// Também conseguimos colocar ela dentro de uma variavel como na função anonima
+
+const showArrow = () => { // Nesse caso apenas tiramos o "function" e usamos apenas o parenteses com o sinal de maior ou igual =>
+  console.log("Olá")
+
+}
+
+console.log(showArrow())
+showArrow()
+
+const showArrow1 = (nameUser) => {
+  console.log(`Ola, ${nameUser}`) // Com interpolação
+}
+
+showArrow1("Lucas")
+console.log(showArrow1("Emilly"))
+//-----------------------------------------------------------------------------------------
+
+// CALLBACK FUNCTION: é uma função passada para outra função como argumento
+function execute(taskName, callBack){
+  console.log("Executando a tarefa: ", taskName)
+  callBack()
+
+}
+function callBack() {
+  console.log("Tarefa Finalizada")
+}
+
+//Passando para a função
+execute("Download do arquivo...", callBack)
+
+//Criando a função no proprio parametro (função anonima)
+execute("Upload do arquivo...", function(){
+  console.log("Função de callback com uma função anonima")
+})
+
+//Utilizando arrow function
+execute("Excluindo arquivo...", () => {
+  console.log("Arquivo excluido!")
+})
